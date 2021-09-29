ddbe9568ea4d35f0296dfddb7a58dbef7d428de6
Aravindh Dasari <66909371+Aravindh-dasari@users.noreply.github.com>
2021-09-29 16:36:42 +0530

    Update main.py

--- diab/main.py
+++ diab/main.py
@@ -7,9 +7,7 @@ from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
 import pickle
 import pred
-#from random import randint
 
-#imports for model pred
 import numpy as np
 import pandas as pd
 pd.set_option('max_columns', None)
@@ -17,15 +15,11 @@ pd.set_option('max_columns', None)
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
 from sklearn.svm import LinearSVC, SVC
-#imports for model pred end
-
 
 app = Flask(__name__)
 app.secret_key = "abc"
 app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
 app.config['UPLOAD_EXTENSIONS'] = ['.csv', '.xlsx']
-#app.config['UPLOAD_PATH'] = '/' 
-
 
 #uploader
 @app.route('/', methods=['GET', 'POST'])
@@ -44,13 +38,6 @@ def index():
         else:    
             return render_template('index.html', msg = "File Upload Failed. Please Try Again!")
     return render_template('index.html', msg = "File not uploaded. Please upload a csv file.")
-
-#Page2
-'''
-@app.route("/page2")
-def page2():
-    return "Hello, Welcome to Page2"
-'''   
 
 if __name__ == "__main__":
     app.run(debug=True)
