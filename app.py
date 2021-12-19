488d5b2db154825ae75d33aae82f249d73b2f98a
Aravindh Dasari <66909371+Aravindh-dasari@users.noreply.github.com>
2021-12-20 00:59:57 +0530

    Added chronic_kidney files

--- app.py
+++ app.py
@@ -2,20 +2,19 @@ from flask import Flask, render_template, url_for, flash, redirect
 import joblib
 from flask import request
 import numpy as np
-import pickle
 
 app = Flask(__name__, template_folder='templates')
 
 @app.route("/")
 
-@app.route("/Diabetes")
+@app.route("/kidney")
 def cancer():
-    return render_template("diabetes.html")
+    return render_template("kidney.html")
 
 def ValuePredictor(to_predict_list, size):
     to_predict = np.array(to_predict_list).reshape(1,size)
-    if(size==6):
-        loaded_model = joblib.load('diabetes_model.pkl')
+    if(size==7):
+        loaded_model = joblib.load(r'kidney_model.pkl')
         result = loaded_model.predict(to_predict)
     return result[0]
 
@@ -26,8 +25,8 @@ def predict():
         to_predict_list = list(to_predict_list.values())
         to_predict_list = list(map(float, to_predict_list))
          #diabetes
-        if(len(to_predict_list)==6):
-            result = ValuePredictor(to_predict_list,6)
+        if(len(to_predict_list)==7):
+            result = ValuePredictor(to_predict_list,7)
     
     if(int(result)==1):
         prediction = "Sorry you chances of getting the disease. Please consult the doctor immediately"
